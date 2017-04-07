01d3548ee25258bb557d4e29e3bbcf1b44fec2aa
groulef <groule_f@etna-alternance.net>
2017-04-07 23:31:52 +0200

    clean

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,6 @@ module ApiEtna
     	format :json
 
 		resource :users do
-
 			http_basic do |username, password|
 				user = 0
 	    		::Constants::DB_CO.query("SELECT * FROM user WHERE email=\"#{username}\"").each do |u|
@@ -27,33 +26,32 @@ module ApiEtna
 	    		end
 	    	end
 
-			desc 'Add a user'
+			desc 'Add a user - Route 1 '
 			post '/' do
 				count = res = 0
 				status_msg = ""
 				status_code = 0
-				email = params[:email].nil? ? "" : ::Constants::DB_CO.escape(params[:email])
+				roles_allowed = ["admin", "normal"]
+				email = params[:email] ?  ::Constants::DB_CO.escape(params[:email]) : res += 1
 				::Constants::DB_CO.query("SELECT * FROM user WHERE email = '#{email}'").each do |e|
 					count += 1
 				end
 
-				if count == 0 && email != ""
-					p params
-					lastname = params[:lastname].nil? == false ? res += 1 : ::Constants::DB_CO.escape(params[:lastname])
-					firstname = params[:firstname].nil? == false ? res += 1 : ::Constants::DB_CO.escape(params[:firstname])
-					role = params[:role].nil? == false ? res += 1 : ::Constants::DB_CO.escape(params[:role])
-					password = params[:password].nil? == false ? res += 1 : ::Constants::DB_CO.escape(params[:password])
-
-					if res != 0
+				lastname = params[:lastname] ? ::Constants::DB_CO.escape(params[:lastname]) : res += 1 
+				firstname = params[:firstname] ? ::Constants::DB_CO.escape(params[:firstname]) : res += 1 
+				role = params[:role] ? ::Constants::DB_CO.escape(params[:role]) : res += 1 
+				password = params[:password] ? ::Constants::DB_CO.escape(params[:password]) : res += 1 
+				if count == 0 && roles_allowed.include?(role)
+					if res == 0
 						::Constants::DB_CO.query("INSERT INTO user (lastname, firstname, email, password, role) VALUES ('#{lastname}', '#{firstname}', '#{email}', '#{password}', '#{role}')")
-						status_msg = "Created User"
-						status_code = 201
+						status_msg = "User successfully created"
+						status_code = 200
 					else
-						status_msg = "ErrorResponse"
+						status_msg = "An error occured"
 						status_code = 400
 					end
 				else
-					status_msg = "ErrorResponse"
+					status_msg = "An error occured"
 					status_code = 400 
 				end
 				error!({"message" => status_msg}, status_code)				
@@ -61,53 +59,91 @@ module ApiEtna
 	    end
 
 	    resource :user do
-	    	desc 'get user'
-	    	get '/:uid' do
-	    		query = "SELECT * FROM user WHERE id = #{params[:uid]}"
-	    		::Constants::DB_CO.query(query).each do |u|
-	    			p u
+	    	http_basic do |username, password|
+				user = 0
+	    		::Constants::DB_CO.query("SELECT * FROM user WHERE email=\"#{username}\"").each do |u|
+	    			user = u;
+	    		end
+	    		pass = Digest::SHA1.hexdigest password
+
+	    		if user != 0 && {user["email"] => user["password"]}[username] == pass
+	    			if  user["role"] == "admin"
+	    				true
+	    			else
+	    				false
+		    		  	error!({"message" => "You must be admin"}, 403)
+	    			end
+	    		else
+	    		  	false
+	    		  	error!({"message" => "You must be connected"}, 401)
 	    		end
 	    	end
 
-		    desc 'modifies a user'
+	    	desc 'get user'
+	    	get '/:uid' do
+	    		query = "SELECT * FROM user WHERE id = #{params[:uid]}"
+	    		user = []
+	    		p query
+	    		::Constants::DB_CO.query(query).each do |u|
+	    			p u
+	    			user = u
+	    		end
+	    		error!(u, 200)
+	    	end
+
+		    desc 'modifies a user - Route 2'
 	      	put '/:uid' do
-	      		count = 0
-	      		::Constants::DB_CO.query("SELECT * FROM user WHERE id = #{params[:uid]}").each do |r|
-	      			if r["id"] != params[:uid]
-	      				count += 1
-	      			end
-	      		end
-	      		allowed_params = ["email", "firstname", "lastname", "password", "role"]
-	      		query = "UPDATE user SET "
-	      		params.each do |key, value|
-	      			p key
-	      			p value
-	      			if allowed_params.include?(key)
-	      				query += " #{key} = '#{value}',"
-	      			end
-	      		end
-	      		query = query.chop
-	      		query += " WHERE id = #{params[:uid]}"
-	      		p query
-	      		# raise
-	      		p params[:uid]
-	      		::Constants::DB_CO.query(query)
-	      		# query += params[:lastname].nil? ? "" : "`lastname` = #{params[:lastname]} "
-	      		# query += params[:firstname].nil? ? "" : "`firstname` = #{params[:firstname]} "
-	      		# query += params[:lastname].nil? ? "" : "`lastname` = #{params[:lastname]}"
-	      		# query += params[:lastname].nil? ? "" : "`lastname` = #{params[:lastname]}"
-	      		# query += params[:lastname].nil? ? "" : "`lastname` = #{params[:lastname]}"
-	      		# UPDATE user SET WHERE id = #{params[:uid]}
-	      		# ::Constants::DB_CO.query("SELECT * FROM user WHERE id = #{params[:uid]}").each do |e|
-	      		# 	lastname 	= params[:lastname].nil? 	? e[1] : params[:lastname]
-	      		# 	firstname 	= params[:firstname].nil? 	? e[2] : params[:firstname]
-	      		# 	email 		= params[:email].nil? 		? e[3] : params[:email]
-	      		# 	#password 	= params[:password].nil? 	? e[2] : Digest::SHA1.hexdigest params[:password]
-	      		# 	role 		= params[:role].nil? 		? e[5] : params[:role]
-	      		# 	p lastname 
-	      		# end
+	      		if ApiEtna::User.check_user(params[:uid]) == 0
+	      			status_msg = "User was not found"
+					status_code = 404
+				else
+					roles_allowed = ["admin", "normal"]
+		      		allowed_params = ["email", "firstname", "lastname", "password", "role"]
+		      		query = "UPDATE user SET "
+		      		params.each do |key, value|
+		      			if allowed_params.include?(key)
+		      				query += " #{key} = '#{::Constants::DB_CO.escape(value)}',"
+		      			end
+		      		end
+		      		query = query.chop
+		      		query += " WHERE id = #{params[:uid]}"
+		      		if params[:role] && !roles_allowed.include?(params[:role])
+						status_msg = "An error occured"
+						status_code = 400 
+		      		else
+		      			status_msg = "User successfully modified"
+		      			status_code = 200
+		      		end
+		      	end
+		      	error!({"message" => status_msg}, status_code)
+		    end
+
+		    desc 'deletes a user - Route 3'
+		    delete '/:uid' do
+	      		if ApiEtna::User.check_user(params[:uid]) == 0
+	      			status_msg = "User was not found"
+					status_code = 404
+				else
+			    	query = "DELETE FROM user WHERE id = #{params[:uid]}"
+			    	::Constants::DB_CO.query(query)
+			    	status_msg = "successfully deleted"
+			    	status_code = 200
+	    		end
+		      	error!({"message" => status_msg}, status_code)
 		    end
 	    end
+	end
+
+	class User
+		def self.check_user uid
+			count = 0
+			::Constants::DB_CO.query("SELECT * FROM user WHERE id = #{uid.to_i}").each do |r|
+      			if r["id"].to_i == uid.to_i
+      				count += 1
+      			end
+	      	end
+			count
+		end
 	end
 end
 
--- config.ru
+++ config.ru
@@ -1,17 +1,7 @@
 require 'grape'
 require 'mysql2'
-# require 'app/controllers/application_controller.rb'
 require File.expand_path('../app/controllers/user_controller', __FILE__)
 require File.expand_path('../app/controllers/application_controller', __FILE__)
 require File.expand_path('../config/initializers', __FILE__)
-
-
-
-# module API
-
-#   class Base < Grape::API
-#     mount ::ApiEtna::API
-#   end
-# end
 
 run ApiEtna::ApplicationController
