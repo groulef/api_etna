745313e86bc965228909095954e863011e0d9b6a
groulef <groule_f@etna-alternance.net>
2017-04-12 10:45:59 +0200

    change http basic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,66 @@ module ApiEtna
 	class UserController < ::Grape::API
     	format :json
 
+    	resource :users do
+			http_basic do |username, password|
+				user = 0
+	    		::Constants::DB_CO.query("SELECT * FROM user WHERE email=\"#{username}\"").each do |u|
+	    			user = u;
+	    		end
+	    		pass = Digest::SHA1.hexdigest password
+
+	    		if user != 0 && {user["email"] => user["password"]}[username] == pass
+    				true
+	    		else
+	    		  	false
+	    		  	error!({"message" => "You must be connected"}, 401)
+	    		end
+	    	end
+
+			desc 'get all users - Route 5'
+			get '/' do
+				p params
+				query = "SELECT * FROM user"
+				users = []
+				::Constants::DB_CO.query(query).each do |u|
+					users << u
+				end
+				status 200
+				users
+			end
+
+			desc 'searh user/s - Route 6'
+			get '/search' do
+				status_code = 0
+				q = params[:q]
+				query = "SELECT id, firstname, lastname, email, role FROM user WHERE lastname LIKE \"%#{q}%\" AND email LIKE \"%#{q}%\""
+				verif_count = true if Float(params[:count]) rescue false
+				p verif_count
+				if params[:count] && verif_count == true
+					query += " LIMIT #{params[:count]}"
+				elsif params[:count] && verif_count == nil
+					status_code = 400
+					status_msg = "An error occurred"
+				end
+				if status_code == 0
+					p query
+					users = []
+					::Constants::DB_CO.query(query).each do |u|
+						users << u
+					end
+					if users == []
+						status_code = 404
+						status_msg = "No user found"
+					else
+						status_msg = users
+						status_code = 200
+					end
+				end
+				status status_code
+				status_msg
+			end 
+		end
+
 		resource :users do
 			http_basic do |username, password|
 				user = 0
@@ -63,47 +123,10 @@ module ApiEtna
 				status status_code
 				status_msg				
 			end
-
-			desc 'get all users - Route 5'
-			get '/' do
-				query = "SELECT * FROM user"
-				users = []
-				::Constants::DB_CO.query(query).each do |u|
-					users << u
-				end
-				status 20
-				users
-			end
-
-			desc 'searh user/s - Route 6'
-			get '/search' do
-				status_code = 0
-				q = params[:q]
-				query = "SELECT id, firstname, lastname, email, role FROM user WHERE lastname LIKE \"%#{q}%\" AND email LIKE \"%#{q}%\""
-				verif_count = params[:count].is_a? Integer
-				p verif_count
-				if params[:count] && verif_count == true
-					query += " LIMIT #{params[:count]}"
-				elsif params[:count] && verif_count == false
-					status_code = 400
-					status_msg = "An error occurred"
-				end
-				if status_code == 0
-					p query
-					users = []
-					::Constants::DB_CO.query(query).each do |u|
-						users << u
-					end
-					status_msg = users
-					status_code = 200
-				end
-				status status_code
-				status_msg
-			end 
 	    end
 
 	    resource :user do
-	    	http_basic do |username, password|
+			http_basic do |username, password|
 				user = 0
 	    		::Constants::DB_CO.query("SELECT * FROM user WHERE email=\"#{username}\"").each do |u|
 	    			user = u;
@@ -111,12 +134,7 @@ module ApiEtna
 	    		pass = Digest::SHA1.hexdigest password
 
 	    		if user != 0 && {user["email"] => user["password"]}[username] == pass
-	    			if  user["role"] == "admin"
-	    				true
-	    			else
-	    				false
-		    		  	error!({"message" => "You must be admin"}, 403)
-	    			end
+    				true
 	    		else
 	    		  	false
 	    		  	error!({"message" => "You must be connected"}, 401)
@@ -140,6 +158,28 @@ module ApiEtna
 		    	end
 	    		status status_code
 	    		status_msg
+	    	end
+	    end
+
+	    resource :user do
+	    	http_basic do |username, password|
+				user = 0
+	    		::Constants::DB_CO.query("SELECT * FROM user WHERE email=\"#{username}\"").each do |u|
+	    			user = u;
+	    		end
+	    		pass = Digest::SHA1.hexdigest password
+
+	    		if user != 0 && {user["email"] => user["password"]}[username] == pass
+	    			if  user["role"] == "admin"
+	    				true
+	    			else
+	    				false
+		    		  	error!({"message" => "You must be admin"}, 403)
+	    			end
+	    		else
+	    		  	false
+	    		  	error!({"message" => "You must be connected"}, 401)
+	    		end
 	    	end
 
 		    desc 'modifies a user - Route 2'
